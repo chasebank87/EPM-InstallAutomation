beb2541ba9d273ac47302f064448e1cf9bef0471
Chase Elder <40812257+chasebank87@users.noreply.github.com>
2019-03-27 17:17:44 -0700

    Update configure900.ps1

--- Powershell/configure900.ps1
+++ Powershell/configure900.ps1
@@ -1271,7 +1271,22 @@ if($inputEPMADB -eq $null) {
         }
     }
 	
-	#announce completion 
+#enderegion
+
+#region export common settings if not remote
+    
+    if($remoteDeployment -ne $true){
+        $commonSettings = @()
+        foreach($i in $dbProperties){
+            [PSObject]$property = Invoke-SQLcmd -ConnectionString "Server=$($dbServer);User Id=$($dbUser);Password=$($dbPassword);Database=$($foundationDB)" -Query "SELECT PROPERTY_NAME,PROPERTY_VALUE FROM HSS_COMPONENT_PROPERTY_VALUES WHERE PROPERTY_NAME = '$($i)'"
+            $commonSettings += $property
+        }
+        $commonSettings | Export-Csv "$($installerPAth)\Temp\commonSettings.csv"
+    }
+
+#endregion
+
+#regeion announce completion 
 	Write-Host '
 
 
@@ -1285,3 +1300,5 @@ if($inputEPMADB -eq $null) {
  ' -ForegroundColor magenta
 
 #endregion
+
+
